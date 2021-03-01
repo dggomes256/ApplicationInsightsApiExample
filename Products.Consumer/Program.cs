501572395859ac36c3baaf0ca2ff234be616f122
Diego Gomes dos Santos Zaratine <diego.zaratine@kroton.com.br>
2021-03-01 18:39:43 -0300

    Implementação basica de consumer

---
+++ Products.Consumer/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Products.Consumer
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+             => await new HostBuilder()
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+                config.AddAzureAppConfiguration(options =>
+                {
+                    options.Connect(configuration["AppConfigurationConnectionString"])
+                           .ConfigureRefresh(refresh =>
+                           {
+                               refresh.Register("ProductsConsumer:Sentinel", refreshAll: true)
+                                      .SetCacheExpiration(new TimeSpan(0, 0, 5));
+                           });
+                });
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging
+                .AddConfiguration(hostingContext.Configuration.GetSection("Logging:LogLevel:Default"))
+                .AddConfiguration(hostingContext.Configuration.GetSection("Logging:ApplicationInsights:LogLevel:Default"))
+                .AddConsole()
+                .AddDebug()
+                .AddApplicationInsights();
+            })
+            .ConfigureServices(services =>
+            {
+                services.AddAzureAppConfiguration();
+                services.AddSingleton<ITelemetryInitializer, CloudRoleNameTelemetryInitializer>();
+                services.AddApplicationInsightsTelemetryWorkerService();
+                services.AddSingleton(services);
+                services.AddApplicationInsightsTelemetryWorkerService();
+                services.AddHostedService<Worker>();
+            }
+                )
+            .RunConsoleAsync();
+        
+
+
+        
+    }
+}
---
+++ Products.Consumer/Worker.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Products.Consumer
+{
+    internal class Worker:  IHostedService, IDisposable
+    {
+        public Worker()
+        {
+        }
+
+        protected Task ExecuteAsync()
+        {
+            //Warmup das filas de envio
+            while(true)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine(DateTime.Now);
+            }
+            
+
+            return Task.CompletedTask;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await ExecuteAsync();
+
+
+
+            while (!cancellationToken.IsCancellationRequested) //Keep it alive
+            {
+                Thread.Sleep(500);
+                Console.WriteLine(DateTime.Now);
+            }
+
+
+            Thread.Sleep(5000);
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            this.Dispose();
+            return Task.CompletedTask;
+        }
+
+        #region IDisposable Support
+
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: set large fields to null.
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+
+        #endregion IDisposable Support
+    }
+}
